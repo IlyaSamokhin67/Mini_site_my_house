08b034b2c28ea5d1286cf5ae87bc5a7166315497
IlyaSamokhin67 <121039212+IlyaSamokhin67@users.noreply.github.com>
2023-02-06 23:22:29 +0300

    Update form.php

--- PHP/form.php
+++ PHP/form.php
@@ -1,7 +1,8 @@
 <?php
 if (isset ($_POST)){
-   print ("Name" . $_POST['fio']);
-   print ("<br>Email:" . $_POST['mail']);
-   print ("<br>Theme:" . $_POST['title']);
-   print ("<br>Message" . $_POST['msg']);
-}
+   echo "Ваша форома отправлена. <br> Наши специалисты свяжуться с вами в течениии 20 минут!";
+   print ("<br>Name: " . $_POST['fio']);
+   print ("<br>Email: " . $_POST['mail']);
+   print ("<br>Theme: " . $_POST['title']);
+   print ("<br>Message: " . $_POST['msg']);
+} 
